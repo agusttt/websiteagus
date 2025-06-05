ad6debaf3440ccbcc26a8b54a4c37e2990663083
agusttt <agussusilo0893@gmail.com>
2025-06-05 12:17:02 +0700

    adding header component

---
+++ src/components/Header.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+interface Props {}
+
+function Header(props: Props) {
+    const {} = props
+
+    return (
+        <div className="w-full">
+            <div className="wrapper"></div>
+        </div>
+    )
+}
+
+export default Header
