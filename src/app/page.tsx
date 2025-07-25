3b747ecbf820164e677f2e357a8538ed08873805
prasojopxl <prasojodesigner@gmail.com>
2025-07-25 17:05:34 +0700

    remove footer

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,20 @@
 import HelloWorld from "@/components/HelloWorld";
+import HeroSection from "@/components/HeroSection";
+import ShowcaseHome from "@/components/ShowcaseHome";
+import Clients from "@/components/Clients";
+import Experiences from "@/components/Experiences";
+import Explorations from "@/components/Explorations";
+import Testimonials from "@/components/Testimonials";
 
 export default function Home() {
-    return (
-        <div className="">
-            <HelloWorld />
-        </div>
-    );
+	return (
+		<div className="">
+			<HeroSection />
+			<Experiences />
+			<ShowcaseHome />
+			<Explorations />
+			<Clients />
+			<Testimonials />
+		</div>
+	);
 }
